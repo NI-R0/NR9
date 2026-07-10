0e4f4ad10e2b3e0f3bf1e020794e885f8e12c81e
lethey2552 <manthey-leon@web.de>
2026-07-10 17:32:33 +0200

    allowing limited jax memory preallocation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false'
+os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'true'
+os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION'] = '0.5'
 
 from loguru import logger
 from src.cli import parse_args
